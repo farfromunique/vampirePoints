8e1df40550e0cd119e7a015dcc17de6dd2a03e17
Aaron C <git@acwpd.com>
2016-09-05 17:57:15 -0700

    Parser::stringToArray was missing
    Too many versions of my files all over :(

--- classes/parser.class.php
+++ classes/parser.class.php
@@ -57,6 +57,29 @@ class Parser {
         }
         return array_values($site_array);
     }
+
+    public function stringToArray($string) {
+        $string = str_replace("\n", "", $string);
+        $output = explode(";",$string);
+        for ($i = 0; $i<(count($output)); $i++) {
+            $output[$i] = explode("=>",$output[$i]);
+            if (array_key_exists(1,$output[$i])) {
+                $output[$i][1] = rtrim($output[$i][1]);
+            };
+        }
+        for ($i = 0; $i<(count($output)-1); $i++) {
+            if (array_key_exists(1,$output[$i])) {
+                $keys[$i] = trim($output[$i][0]);
+                $values[$i] = $output[$i][1];
+            }
+        };
+        
+        $output = array_fill_keys($keys, "x");
+        for ($i=0; $i<count($output); $i++) {
+            $output[$keys[$i]] = $values[$i];
+        }
+        return $output;
+    }
 }
 
 ?>
