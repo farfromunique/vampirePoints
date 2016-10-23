517b21f93e5f9c7a5e33da1a22be94b7c5f8d808
Aaron C <git@acwpd.com>
2016-10-22 23:07:42 -0700

    Rebuilding the front and back ends. Class: DotRating    supplants Classes Dot, Attribute, AttributeGroup, using a more streamlined approach.
     the rest is just getting layout right, and making sure I can use DotRating effectively.
    
     TODO: Finish sidebar
     TODO: Extract Sidebar to new file
     TODO: Style Print view
     TODO: Update BloodPool and Blood Classes

--- classes/parser.class.php
+++ classes/parser.class.php
@@ -36,50 +36,54 @@ class Parser {
 	public function makeMainDiv($str) {
 		return "<div id='content' class='content'>$str</div>";
 	}
-
-    public function buildOutput($pageName) {
-        ob_start();
-        require($pageName);
-        $output = ob_get_contents();
-        ob_end_clean();
-        return $output;
-    }
-
-    public function getParameters($uri) {
-        $uri = strtolower($uri);
-        $request  = str_replace("", "", $uri);
-        $site_array  = mb_split("/", $request);
-
-        foreach($site_array as $key => $value) {
-            if($value == "") {
-                unset($site_array[$key]);
-            }
-        }
-        return array_values($site_array);
-    }
-
-    public function stringToArray($string) {
-        $string = str_replace("\n", "", $string);
-        $output = explode(";",$string);
-        for ($i = 0; $i<(count($output)); $i++) {
-            $output[$i] = explode("=>",$output[$i]);
-            if (array_key_exists(1,$output[$i])) {
-                $output[$i][1] = rtrim($output[$i][1]);
-            };
-        }
-        for ($i = 0; $i<(count($output)-1); $i++) {
-            if (array_key_exists(1,$output[$i])) {
-                $keys[$i] = trim($output[$i][0]);
-                $values[$i] = $output[$i][1];
-            }
-        };
-        
-        $output = array_fill_keys($keys, "x");
-        for ($i=0; $i<count($output); $i++) {
-            $output[$keys[$i]] = $values[$i];
-        }
-        return $output;
-    }
+	
+	public function buildOutput($pageName)
+	{
+		ob_start();
+		require($pageName);
+		$output = ob_get_contents();
+		ob_end_clean();
+		return $output;
+	}
+	
+	public function getParameters($uri)
+	{
+		$uri = strtolower($uri);
+		$request = str_replace("", "", $uri);
+		$site_array = mb_split("/", $request);
+		
+		foreach ($site_array as $key => $value) {
+			if ($value == "") {
+				unset($site_array[$key]);
+			}
+		}
+		return array_values($site_array);
+	}
+	
+	public function stringToArray($string)
+	{
+		$string = str_replace("\n", "", $string);
+		$output = explode(";", $string);
+		for ($i = 0; $i < (count($output)); $i++) {
+			$output[$i] = explode("=>", $output[$i]);
+			if (array_key_exists(1, $output[$i])) {
+				$output[$i][1] = rtrim($output[$i][1]);
+			};
+		}
+		for ($i = 0; $i < (count($output) - 1); $i++) {
+			if (array_key_exists(1, $output[$i])) {
+				$keys[$i] = trim($output[$i][0]);
+				$values[$i] = $output[$i][1];
+			}
+		};
+		
+		$output = array_fill_keys($keys, "x");
+		for ($i = 0; $i < count($output); $i++) {
+			$output[$keys[$i]] = $values[$i];
+		}
+		return $output;
+	}
+	
 }
 
 ?>
