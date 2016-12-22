5ca041879cc037299b3fec506090113e22e8cac2
Aaron C <aaron@acwpd.com>
2016-12-22 11:39:07 -0800

    config2

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	if ($_SERVER['SERVER_NAME'] == 'vampire.acwpd.com' 
 	 || $_SERVER['SERVER_NAME'] == 'vampirepoints.nfshost.com') { // put your PRODUCTION server name(s) here
 		define('VAMPIRE_ENVIRONMENT', 'Production');
-		define('VAMPIRE_DB_HOST', 'vampirePoints.db');				// use the servername from your production environment
+		define('VAMPIRE_DB_HOST', 'vampirepoints.db');				// use the servername from your production environment
 	} else {
 		define('VAMPIRE_ENVIRONMENT', 'Development');
 		define('VAMPIRE_DB_HOST', 'localhost');				// user the servername from your development / staging environment
