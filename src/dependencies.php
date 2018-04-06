0204f47d3c447fa02e315596d5b91f33fa28d529
Aaron C <aaron@acwpd.com>
2018-04-05 19:43:07 -0700

    Add a (simple) custom 404

--- src/dependencies.php
+++ src/dependencies.php
@@ -17,3 +17,15 @@ $container['logger'] = function ($c) {
     $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
     return $logger;
 };
+
+// Custom 404
+$container['notFoundHandler'] = function ($container) {
+    return function ($request, $response) use ($container) {
+		$container->logger->error("404 - " . $container['request']->getRequestTarget());
+		return $container['response']
+            ->withStatus(404)
+            ->withHeader('Content-Type', 'text/html')
+			->write('Page not found.<br />' . "\n" . 'Path requested: ' . $container['request']->getRequestTarget());
+			
+    };
+};
