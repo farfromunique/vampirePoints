6b1e834ff3e295d1af89f03c3583fe14bf63d23e
Aaron C <aaron@acwpd.com>
2018-04-05 19:42:32 -0700

    Moving static design pages to a logical place, and adding routes

--- src/routes.php
+++ src/routes.php
@@ -5,15 +5,26 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/', function ($request, $response, $args) {
-	$this->logger->addInfo("Root");
-	return $response->withStatus(200)->write('Hello World!');
-});
-
-$app->get('/{name}', function (Request $request, Response $response, array $args) {
+$app->any('/', function (Request $request, Response $response, $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+	$this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    return $this->renderer->render($response, 'index.phtml', ['thing' => 'that']);
+});
+
+$app->any('/static/sheet', function (Request $request, Response $response, array $args) {
+    // Sample log message
+	$this->logger->info("BS4 Sheet page");
+
+    // Render index view
+    return $this->renderer->render($response, 'sheet.phtml', $args);
+});
+
+$app->any('/static/home', function (Request $request, Response $response, array $args) {
+    // Sample log message
+	$this->logger->info("BS4 Homepage");
+
+    // Render index view
+	return $this->renderer->render($response, 'home.phtml', $args);
 });
